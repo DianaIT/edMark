5b4ece697948dbddc36c45b3d3fe0a634c98852f
Diana <dianahdezsoler@gmail.com>
2020-08-21 17:17:49 +0200

    authDomain updated

--- firebase/config.js
+++ firebase/config.js
@@ -1,6 +1,6 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyDPynLol0aOcPnTWOVF8m-ns2jTHhhuQ2E",
-  authDomain: "https://edmark.vercel.app/",
+  authDomain: "edmark-649ec.firebaseapp.com",
   databaseURL: "https://edmark-649ec.firebaseio.com",
   projectId: "edmark-649ec",
   storageBucket: "edmark-649ec.appspot.com",
